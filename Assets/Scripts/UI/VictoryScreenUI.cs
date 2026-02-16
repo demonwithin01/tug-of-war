4906109feee7bee21bdb744ac244bbb7995d8093
demonwithin01 <stephenbarnes88@gmail.com>
2026-02-16 21:11:16 +1100

    Added a purchased asset as well as a quick victory screen

---
+++ Assets/Scripts/UI/VictoryScreenUI.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+public class VictoryScreenUI : MonoBehaviour
+{
+    [SerializeField]
+    private VictoryLocation victoryLocation;
+
+    /// <summary>
+    /// Unity start method. Ensures the UI is hidden at the start of the game.
+    /// </summary>
+    private void Start()
+    {
+        // Ensure the UI is hidden at the start of the game.
+        Hide();
+
+        // Register the event handler for when the victory location is reached.
+        this.victoryLocation.OnVictoryReached += VictoryLocation_OnVictoryReached;
+    }
+
+    /// <summary>
+    /// Handler for when the victory location is reached. Shows the victory screen UI.
+    /// </summary>
+    private void VictoryLocation_OnVictoryReached(object sender, EventArgs e)
+    {
+        Show();
+    }
+
+    /// <summary>
+    /// Shows the victory screen UI.
+    /// </summary>
+    private void Show()
+    {
+        
+        // Activate the UI.
+        gameObject.SetActive( true );
+    }
+
+    /// <summary>
+    /// Hides the victory screen UI.
+    /// </summary>
+    private void Hide()
+    {
+        // Deactivate the UI.
+        gameObject.SetActive( false );
+    }
+}
